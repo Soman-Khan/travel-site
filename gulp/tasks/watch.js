a95e3cb56b73b86e4a8455746fa803aede1a2f64
Soman Khan <soman480@diu.edu.bd>
2018-07-15 12:01:58 +0600

    completed large hero block and intro content block

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+	watch = require('gulp-watch'),
+	browserSync = require('browser-sync').create();
+
+
+gulp.task('watch',function(){
+
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "app"
+		}
+	});
+
+	watch("./app/index.html",function(){
+		browserSync.reload();
+	});
+
+	watch('./app/assets/styles/**/*.css',function(){
+		//gulp.start('styles'); ##style will be add as injectCss dependendies
+		gulp.start('injectCss');
+	});
+});
+
+
+
+gulp.task('injectCss', ['styles'], function(){
+	return gulp.src('./app/temp/styles/styles.css')
+		.pipe(browserSync.stream());
+});
